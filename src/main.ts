a0b5dca984e9add9c14871a86fbe0ec1efa575c9
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2024-06-04 10:24:36 +0300

    cors setup prod

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('planner_api')
 	app.use(cookieParser())
-	app.enableCors()
+	app.enableCors({
+		origin: process.env.CORS_ORIGIN.split(','),
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	})
 	await app.listen(parseInt(process.env.PORT, 10) || 4200);
 }
 bootstrap()
