b793219f2aa616b8f117a2a2fac0fd0f5a5c19a1
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2024-06-04 18:27:25 +0300

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,17 +9,22 @@ import { UserService } from 'src/user/user.service'
 import { AuthDto } from './dto/auth.dto'
 import { verify } from 'argon2'
 import { Response } from 'express'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
 	REFRESH_TOKEN_NAME = 'refreshToken'
 	EXPIRE_DAY_REFRESH_TOKEN = 1
-
+	REFRESH_TOKEN_DOMAIN: string
 
 	constructor(
 		private jwt: JwtService,
 		private userService: UserService,
+		private configService: ConfigService
 	) {
+		this.REFRESH_TOKEN_DOMAIN = this.configService.get<string>(
+			'REFRESH_TOKEN_DOMAIN'
+		)
 	}
 	async login(dto: AuthDto) {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -69,11 +74,13 @@ export class AuthService {
 	addRefreshTokenToResponse(res: Response, refreshToken: string) {
 		const expiresIn = new Date()
 		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+		console.log(this.REFRESH_TOKEN_DOMAIN)
 		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
 			httpOnly: true,
 			expires: expiresIn,
 			secure: true,
-			sameSite: "lax",
+			sameSite: 'lax',
+			domain: this.REFRESH_TOKEN_DOMAIN
 		})
 	}
 
@@ -82,7 +89,8 @@ export class AuthService {
 			httpOnly: true,
 			expires: new Date(0),
 			secure: true,
-			sameSite: "lax",
+			sameSite: 'lax',
+			domain: this.REFRESH_TOKEN_DOMAIN
 		})
 	}
 }
